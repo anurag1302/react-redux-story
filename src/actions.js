485a03736883a2c2009b38209049d5a7799e031a
Anurag <anurag3487@gmail.com>
2024-10-04 22:37:18 +0530

    create redux store

---
+++ src/actions.js
@@ -0,0 +1,15 @@
+// actions.js
+export const addTask = (task) => ({
+  type: "ADD_TASK",
+  payload: task,
+});
+
+export const toggleTask = (taskId) => ({
+  type: "TOGGLE_TASK",
+  payload: taskId,
+});
+
+export const deleteTask = (taskId) => ({
+  type: "DELETE_TASK",
+  payload: taskId,
+});
